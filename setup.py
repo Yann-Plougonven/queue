cf6ed42c1cda80486bda5bf63788ad01ee5980d3
Yann Plougonven <plougonven-y@saint-louis29.net>
2022-12-13 14:30:14 +0100

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Queue',
+    version='1.1',
+    url='https://github.com/Yann-Plougonven/queue',
+    author='Yann Plougonven--Lastennet',
+    author_email='plougonven-y@saint-louis29.net',
+    description="A repository which contains a queue's implementation, made during NSI class",
+    packages=find_packages(),    
+    install_requires=[],
+)
